0bb41b77bee6da103469d42cb0b55d450e6d08d5
KelvinMwinuka <KelvinMwinuka@hotmail.co.uk>
2019-12-29 00:49:45 +0200

    Created and defined routes to check for email and username availability

--- app/Routes/api/validation.php
+++ app/Routes/api/validation.php
@@ -9,10 +9,37 @@ use Slim\App;
 return function(App $app){
     
     $app->get('/api/validate/email', function(Request $req, Response $res){
+       
         // Check wether the email is available
+        $email = $req->getQueryParams()['email'];
+        $user = $this->get('mongodb')->users->findOne(['email' => $email]);
+
+        $response_object = array();
+
+        if($user == null){
+            $response_object['available'] = true;
+        } else {
+            $response_object['available'] = false;
+        }
+
+        $res->getBody()->write(json_encode($response_object));
+        return $res;
     });
 
     $app->get('/api/validate/username', function(Request $req, Response $res){
+        
         // Check whether the username is available
+        $username = $req->getQueryParams()['username'];
+        $user = $this->get('mongodb')->users->findOne(['username' => $username]);
+
+        $response_object = array();
+        if($user == null){
+            $response_object['available'] = true;
+        } else {
+            $response_object['available'] = false;
+        }
+
+        $res->getBody()->write(json_encode($response_object));
+        return $res;
     });
 };
