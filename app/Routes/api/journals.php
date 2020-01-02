d3ea4c66be3e5d144e171f18ff452fc7500e3d80
KelvinMwinuka <KelvinMwinuka@hotmail.co.uk>
2020-01-02 16:08:05 +0200

    Defined journal retrieval route

--- app/Routes/api/journals.php
+++ app/Routes/api/journals.php
@@ -50,8 +50,42 @@ return function(App $app){
     });
 
     $app->get('/api/journals/retrieve', function(Request $req, Response $res){
+        $params = $req->getQueryParams();
+
+        file_put_contents('test.txt', json_encode($params));
+
+        if(!array_key_exists('user_id', $params)){
+            $res->getBody()->write(json_encode([
+                'status' => 'Failed',
+                'message' => 'Cannot retrieve journal(s) for null user'
+            ]));
+            return $res;
+        }
+
         // Return specified journal if it belongs to specified user
+        if(array_key_exists('journal_id', $params)){
+            $journal = $this->get('mongodb')->journals->findOne([
+                '_id' => new MongoDB\BSON\ObjectID($params['journal_id']),
+                'user_id' => new MongoDB\BSON\ObjectID($params['user_id'])
+            ]);
+            $res->getBody()->write(json_encode($journal));
+            return $res;
+        }
+
         // If journal isn't specified, return all journals for this user
+        $cursor = $this->get('mongodb')->journals->find([
+            'user_id' => new MongoDB\BSON\ObjectID($params['user_id'])
+        ]);
+
+        $documents = array();
+
+        foreach($cursor as $document){
+            $document['_id'] = (string)$document['_id'];  // Convert document ID to string before returning
+            array_push($documents, $document);
+        }
+
+        $res->getBody()->write(json_encode($documents));
+        return $res;
     });
 
     $app->post('/api/journals/update', function(Request $req, Response $res){
