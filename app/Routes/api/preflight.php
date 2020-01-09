f842e382d13c2e3b7c2ac14c72deed09045b8c47
KelvinMwinuka <KelvinMwinuka@hotmail.co.uk>
2020-01-09 20:46:26 +0200

    Updated preflight for username validation

--- app/Routes/api/preflight.php
+++ app/Routes/api/preflight.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 return function(App $app){
-    $app->options('/api/validate/username/{username:.+}', function(Request $req, Response $res){
+    $app->options('/api/validate/username?{username:.+}', function(Request $req, Response $res){
         $res->withHeader('Access-Control-Allow-Origin', '*');
         $res->withStatus(200);
         return $res;
