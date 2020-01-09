af3f4a80e172fa9991aeb1f9634c50a80353c831
KelvinMwinuka <KelvinMwinuka@hotmail.co.uk>
2020-01-09 19:46:00 +0200

    Added preflight response headers for email and username validation routes

--- app/Routes/api/preflight.php
+++ app/Routes/api/preflight.php
@@ -7,6 +7,17 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 return function(App $app){
+
+    $app->option('/api/validate/email', function(Request $req, Response $res){
+        $res->withHeader('Access-Control-Allow-Origin', '*');
+        return $res;
+    });
+
+    $app->option('/api/validate/username', function(Request $req, Response $res){
+        $res->withHeader('Access-Control-Allow-Origin', '*');
+        return $res;
+    });
+
     $app->options('/api/user/register', function (Request $req, Response $res){
         $res->withHeader('Access-Control-Allow-Origin', '*');
         return $res;
